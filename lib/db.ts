8ce1eecd11928bfb941bf4f2c01947f84bdef2b6
uvasishtha@gmail.com <uvasishtha@gmail.com>
2026-08-11 18:33:44 -0700

    x

--- lib/db.ts
+++ lib/db.ts
@@ -1,5 +1,18 @@
 import { Pool } from "pg";
 
+// Clean up any old ?sslmode= params from the URL string
+// to prevent the pg driver security warning
+const connectionString = process.env.DATABASE_URL?.replace(
+  /\?sslmode=(prefer|require|verify-ca)/,
+  ""
+);
+
 export const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
+  connectionString,
+  ssl:
+    process.env.NODE_ENV === "production"
+      ? {
+          rejectUnauthorized: false, // Required for most managed cloud Postgres DBs
+        }
+      : false,
 });
